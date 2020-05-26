9f06b2bd50399ce298b840a4d28f9314cec5024d
anuva04 <anuvacotton2016@gmail.com>
2020-05-26 16:11:55 +0530

    components-1

---
+++ src/components/MenuComponent.js
@@ -0,0 +1,77 @@
+import React, { Component } from 'react';
+import {Media} from 'reactstrap';
+
+class Menu extends Component {
+    constructor(props){
+        super(props);
+        this.state = {
+            dishes: [
+                {
+                    id: 0,
+                    name: 'Uthappizza',
+                    image: 'assets/images/uthappizza.png',
+                    category: 'mains',
+                    label: 'Hot',
+                    prize: '4.99',
+                    description: 'A unique combo of uthappam and pizza'
+                },
+                {
+                    id: 1,
+                    name: 'Zucchipakoda',
+                    image: 'assets/images/zucchipakoda.png',
+                    category: 'appetizer',
+                    label: '',
+                    price: '1.99',
+                    description: 'Deep fried zucchini'
+                },
+                {
+                    id: 2,
+                    name: 'Vadonut',
+                    image: 'assets/images/vadonut.png',
+                    category: 'appetizer',
+                    label: 'New',
+                    price: '1.99',
+                    description: 'A quintessential confusion'
+                },
+                {
+                    id: 3,
+                    name: 'ElaiCheese Cake',
+                    image: 'assets/images/elaicheesecake.png',
+                    category: 'dessert',
+                    label: '',
+                    price: '2.99',
+                    description: 'A delectable smi-sweet dish'
+                }
+            ]
+        }
+    }
+    render(){
+        const menu = this.state.dishes.map((dish) => {
+            return (
+                <div key={dish.id} className="col-12 mt-5">
+                    <Media tag="li">
+                        <Media left middle>
+                            <Media object src={dish.image} alt={dish.name}/>
+                        </Media>
+                        <Media body className="ml-5">
+                            <Media heading>{dish.name}</Media>
+                            <p>{dish.description}</p>
+                        </Media>
+                    </Media>
+                </div>
+            );
+        });
+
+        return(
+            <div className="container">
+                <div className="row">
+                    <Media list>
+                        {menu}
+                    </Media>
+                </div>
+            </div>
+        );
+    }
+}
+
+export default Menu;
