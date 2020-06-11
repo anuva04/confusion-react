53e6129c0701721db59c4ab46b56ed3d59c19983
anuva04 <57056078+anuva04@users.noreply.github.com>
2020-06-11 14:02:24 +0530

    about component

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -2,7 +2,8 @@ import React, { Component } from 'react';
 import Menu from './MenuComponent';
 import Home from './HomeComponent';
 import Contact from './ContactComponent';
-import Dishdetail from './DishdetailComponent'
+import Dishdetail from './DishdetailComponent';
+import About from './AboutComponent';
 import Header from './HeaderComponent';
 import Footer from './FooterComponent';
 import {DISHES} from '../shared/dishes';
@@ -46,6 +47,7 @@ class Main extends Component {
         <Header />
         <Switch>
           <Route path="/home" component={HomePage} />
+          <Route path="/aboutus" component={() => <About leaders={this.state.leaders} />} />
           <Route exact path="/menu" component={() => <Menu dishes={this.state.dishes} />} />
           <Route path="/menu/:dishId" component={DishWithId} />
           <Route exact path="/contactus" component={Contact} />
